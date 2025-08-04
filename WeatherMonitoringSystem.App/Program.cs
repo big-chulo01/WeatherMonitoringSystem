480e728175ccf3514e7f0c29e7b057b66414b102
Darlington Mboma <darlingtonwealth0213@gmail.com>
2025-08-04 22:17:59 +0000

    program.cs update

--- WeatherMonitoringSystem.App/Program.cs
+++ WeatherMonitoringSystem.App/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using WeatherMonitoringSystem.Core.Models;
+using WeatherMonitoringSystem.Core.Interfaces;
+using WeatherMonitoringSystem.Core.Models.Display;
+
+// Create displays using the factory
+IDisplay currentDisplay = WeatherStation.CreateCurrentConditionsDisplay(showDateTime: true);
+IDisplay statisticsDisplay = WeatherStation.CreateStatisticsDisplay();
+IDisplay forecastDisplay = WeatherStation.CreateForecastDisplay();
+
+// Get the singleton instance of WeatherData
+WeatherData weatherData = WeatherData.Instance;
+
+// Simulate weather changes
+Random random = new Random();
+for (int i = 0; i < 5; i++)
+{
+    float temp = random.Next(10, 30); // Random temperature between 10°C and 30°C
+    float humidity = random.Next(30, 90); // Random humidity between 30% and 90%
+    float pressure = random.Next(990, 1020); // Random pressure between 990 and 1020 hPa
+    
+    Console.WriteLine($"\n--- Weather Update {i+1} ---");
+    weatherData.SetMeasurements(temp, humidity, pressure);
+    
+    // Wait between updates
+    Thread.Sleep(2000);
+}
